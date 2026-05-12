263011a168ae3c9179b30d12be82f8ffd39c21b1
Alexander <alexander@no-reply.com>
2026-05-13 01:02:41 +0300

    update +docker install

--- setup-main.sh
+++ setup-main.sh
@@ -43,6 +43,8 @@ EOF
     sudo apt update
 
     sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+    sudo usermod -aG docker $USER
+
 }
 
 main (){
